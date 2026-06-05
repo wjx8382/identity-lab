b81e36aa50d97a63a0f07db615c54b93e633b986
吴健雄 <wujianxiong@wondersgroup.com>
2026-06-05 11:02:09 +0800

    init: create auth server project

---
+++ src/main/java/com/wjx/identity/IdentityLabApplication.java
@@ -0,0 +1,13 @@
+package com.wjx.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityLabApplication.class, args);
+	}
+
+}
